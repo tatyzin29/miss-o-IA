62573705170c5487de5614e7060734bc02c2d9ff
tatyzin29 <110253746+tatyzin29@users.noreply.github.com>
2024-08-02 11:09:27 -0300

    02/08/2024

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const caixaPrincipal = document.querySelector(".caixa-principal");
+ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
@@ -10,11 +10,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Emissão de gases de efeito estufa",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                afirmacao: "A crescente conscientização ambiental tem inspirado a implementação de políticas públicas voltadas para a sustentabilidade, promovendo a conservação de ecossistemas e a biodiversidade. Tecnologias renováveis, como energia solar e reciclagem, estão reduzindo significativamente as emissões de carbono. A pesquisa científica continua a oferecer soluções inovadoras para enfrentar desafios ambientais globais."
             },
             {
                 texto: "Uso excessivo de plásticos",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                afirmacao: ""
             }
         ]
     },
@@ -23,11 +23,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Desmatamento indiscriminado",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                afirmacao: ""
             },
             {
                 texto: "Uso responsável de recursos naturais",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                afirmacao: ""
             }
         ]
     },
@@ -36,11 +36,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Energia solar",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                afirmacao: ""
             },
             {
                 texto: "Combustíveis fósseis",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                afirmacao: ""
             }
         ]
     },
@@ -49,11 +49,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Escassez de recursos hídricos",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                afirmacao: ""
             },
             {
                 texto: "Aumento da fertilidade dos ecossistemas aquáticos",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                afirmacao: ""
             }
         ]
     },
@@ -62,11 +62,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Usando transporte público em vez de carros individuais",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                afirmacao: ""
             },
             {
                 texto: "Aumentando o consumo de produtos descartáveis",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                afirmacao: " "
             }
         ]
     },
@@ -105,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Em resumo";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
